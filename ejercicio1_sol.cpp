ac5701f98263d4fb7e7b0eebad2cb1a820601d37
Maria Belen Becerra <124795812+MaBelenBecerra@users.noreply.github.com>
2023-03-06 10:03:53 -0400

    Ejercicio1 practica

---
+++ ejercicio1_sol.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+ using namespace std;
+int main() {
+  
+    // Write C++ code here
+    double number = 6.50;
+    double number2 = 2.50;
+    cout << number + number2 << endl;
+ 
+    
+    return 0;
+}
