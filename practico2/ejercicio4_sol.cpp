1ca48064f160d6481f202fb1e7f0882968b69e3a
Maria Belen Becerra <124795812+MaBelenBecerra@users.noreply.github.com>
2023-03-17 11:23:12 -0400

    Create ejercicio4_sol.cpp

---
+++ practico2/ejercicio4_sol.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+  
+    int x, y, z;
+
+    cin >> x >> y;
+    z = 1;
+    cout << "Your answer: "<<endl;
+    cout << x + y + z;
+
+    return 0;
+}
