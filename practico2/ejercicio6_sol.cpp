a4242cfbcb450798adf4f530aa89a3ef7ebc7440
Maria Belen Becerra <124795812+MaBelenBecerra@users.noreply.github.com>
2023-03-17 11:24:31 -0400

    Create ejercicio6_sol.cpp

---
+++ practico2/ejercicio6_sol.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    
+    string name = "Kendra";
+    string apellido = "Ramirez";
+    cin>> name >> apellido;
+    cout << apellido + ", " +name <<endl;
+
+    return 0;
+}
