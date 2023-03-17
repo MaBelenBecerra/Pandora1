bc6477e4fdd67d1de4b8705169ed314d4a19dc0a
Maria Belen Becerra <124795812+MaBelenBecerra@users.noreply.github.com>
2023-03-17 11:22:23 -0400

    Update and rename practico 2/ejercicio3_sol.cpp to practico2/ejercicio3_sol.cpp

---
+++ practico2/ejercicio3_sol.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+  
+    double width = 5;
+    double height = 7;
+ cout << "Enter rectangle width and height"s << endl;
+ cin >> width >> height;
+ cout << "Rectangle area is "s << width * height << endl;
+ 
+    return 0;
+}
+
