65314c82c1f6f1f2bd5e77072cccd64218599677
Maria Belen Becerra <124795812+MaBelenBecerra@users.noreply.github.com>
2023-03-17 11:25:22 -0400

    Create ejercicio7_sol.cpp

---
+++ practico2/ejercicio7_sol.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // una vaina loca que me lleva a la gloria..
+    
+    string full_name, address, books;
+    getline(cin, full_name);
+    getline(cin, address);
+    getline(cin, books);
+    cout << books <<endl<< address<<endl<< full_name<< endl;
+
+    return 0;
+}
