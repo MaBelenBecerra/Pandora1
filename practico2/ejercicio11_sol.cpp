f2031adce1b408f6bd16a29c3c01fdf859922253
Maria Belen Becerra <124795812+MaBelenBecerra@users.noreply.github.com>
2023-03-18 19:22:01 -0400

    Create ejercicio11_sol.cpp

---
+++ practico2/ejercicio11_sol.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    //And I wanna kiss you, make you feel all right
+    //I'm just so tired to share my nights
+    //I wanna cry and I wanna love
+    //But all my tears have been used up
+    //On another love, another love
+   int collar;
+        cin>>collar;
+        if (collar==11235813)
+        {
+            cout<<"Se encontró a Fibo :)";
+        }else{
+            cout<<"Este no es Fibo :/";
+        }
+
+    return 0;
+}
