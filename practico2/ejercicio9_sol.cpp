abed91afb8ee65ea50bcddf2fe34321cd7fff5d4
Maria Belen Becerra <124795812+MaBelenBecerra@users.noreply.github.com>
2023-03-18 19:07:32 -0400

    Create ejercicio9_sol.cpp

---
+++ practico2/ejercicio9_sol.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    //Secrets I have held in my heart
+    //Are harder to hide than I thought
+    //Maybe I just wanna be yours
+    //Wanna be yours
+    int num;
+        string doc;
+        cin>>num;
+        string numero=to_string(num);
+        doc=numero+" documentos encontrados."s;
+        cout<<doc;
+
+    return 0;
+}
