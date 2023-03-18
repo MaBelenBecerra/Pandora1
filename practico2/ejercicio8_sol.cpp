6d4bc458c55efb1a79f85f919f46ecf8dc76c181
Maria Belen Becerra <124795812+MaBelenBecerra@users.noreply.github.com>
2023-03-18 19:02:55 -0400

    Create ejercicio8_sol.cpp

---
+++ practico2/ejercicio8_sol.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    //'Cause everybody's gotta die sometime, yeah. But baby, don't cry
+    string lan1,lan2,lan3,lan4,lan5;
+    cout<<"Ingrese 5 palabras en una línea por favor :D"<<endl;
+    cin>>lan1>>lan2>>lan3>>lan4>>lan5;
+    char pab1 = lan1[0];
+    char pab2 = lan2[0];
+    char pab3 = lan3[0];
+    char pab4 = lan4[0];
+    char pab5 = lan5[0];
+    cout<<pab1<<pab2<<pab3<<pab4<<pab5<<endl;
+
+
+    return 0;
+}
