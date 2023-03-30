add38490cd607fcc24c8ed377ec41d91ead300d2
Maria Belen Becerra <124795812+MaBelenBecerra@users.noreply.github.com>
2023-03-30 14:54:04 -0400

    Update ejercicio1_sol.cpp

--- practico_3/ejercicio1_sol.cpp
+++ practico_3/ejercicio1_sol.cpp
@@ -1,1 +1,32 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    //Lässt dich hart zu Boden geh'n
+    //Und die Welt zählt laut bis zehn
+    
+    string cAsa1,cAsa2,cAsa3,cAsa;
+    cin>>cAsa1>>cAsa2>>cAsa3>>cAsa;
+    if ((cAsa1!=cAsa2)&&(cAsa2!=cAsa3)&&(cAsa1!=cAsa3))
+    {
+    if (cAsa1==cAsa)
+    {
+        cout<<"Entrar a la casa 1"<<endl;
+    }
+    else if (cAsa2==cAsa)
+    {
+        cout<<"Entrar a la casa 2"<<endl;
+    }
+    else if (cAsa3==cAsa)
+    {
+        cout<<"Entrar a la casa 3"<<endl;
+    }
+    else
+    {
+        cout<<"Busca en otro lugar"<<endl;
+    }
+    }
+    else{
+        cout<<"No hay casas de colores similares";
+    }
+    return 0;
+}
