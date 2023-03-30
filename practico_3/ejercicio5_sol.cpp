8827c871f8332aee7447d983fc3a8023ee288eb3
Maria Belen Becerra <124795812+MaBelenBecerra@users.noreply.github.com>
2023-03-30 15:50:43 -0400

    Create ejercicio5_sol.cpp

---
+++ practico_3/ejercicio5_sol.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    //Que estamos arriba, que esto es otra liga, digan lo que digan somo' lo mejor
+    //Tú y yo brillamos en el cielo como estrellas, corazón
+     int w,z;
+    cin>>w>>z;
+    for (int i=w; ((i>=0)&&(i<=z));++i)
+    {
+        cout<<i<<endl;
+    }
+    return 0;
+}
