faf87157162d39ac622fb0270fb824f299210fc1
Maria Belen Becerra <124795812+MaBelenBecerra@users.noreply.github.com>
2023-03-30 15:15:10 -0400

    Create ejercicio4_sol.cpp

---
+++ practico_3/ejercicio4_sol.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+  //Quiero desnudar tu alma y dedicarle una canción
+  //Darle todo mi cariño y entregarle mi amor
+    int a = 5;
+    int b;
+    do {
+    cout << "Guess the number: "s << endl;
+     cin >> b;
+     } while (a != b);
+    cout << "You are right! :)"s << endl;
+    return 0;
+}
