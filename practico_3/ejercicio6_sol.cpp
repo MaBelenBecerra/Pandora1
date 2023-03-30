3e12e62737b3ca598855d0f4670b26e051f880e1
Maria Belen Becerra <124795812+MaBelenBecerra@users.noreply.github.com>
2023-03-30 15:53:02 -0400

    iNCOMPLETO

---
+++ practico_3/ejercicio6_sol.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    //No estaba esperando nada
+    //De encontrarme contigo anoche
+    //Solo era una más
+    //Una noche linda, algo especial
+}
