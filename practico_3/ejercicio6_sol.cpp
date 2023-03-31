a9cd6ced836f0037481a7b939354c833f124155b
Maria Belen Becerra <124795812+MaBelenBecerra@users.noreply.github.com>
2023-03-31 09:41:20 -0400

    Sim

--- practico_3/ejercicio6_sol.cpp
+++ practico_3/ejercicio6_sol.cpp
@@ -5,4 +5,20 @@ int main() {
     //De encontrarme contigo anoche
     //Solo era una más
     //Una noche linda, algo especial
+    int jake,tonowari;
+    cin>>jake>>tonowari;
+    if (jake<tonowari)
+    {
+    for (int i=jake;i<=tonowari;++i)
+    {
+        cout<<i<<endl;
+    }}
+    else if (jake>tonowari)
+    {
+    for (int i=jake;i>=tonowari;--i)
+    {
+        cout<<i<<endl;
+    }}
+    
+    return 0;
 }
