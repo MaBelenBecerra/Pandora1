5359e53f1c56f1f39e3f78163f5db51c057d9238
Maria Belen Becerra <124795812+MaBelenBecerra@users.noreply.github.com>
2023-04-03 10:43:11 -0400

    Update ejercicio11_sol.cpp

--- practico_3/ejercicio11_sol.cpp
+++ practico_3/ejercicio11_sol.cpp
@@ -1,1 +1,31 @@
  
+#include <iostream>
+using namespace std;
+int main() {
+    //
+    int a,mult;
+    cin>>a;
+    int fact=1;
+    while (a<0)
+    {
+        cout<<"No estan permitido numeros negativos, intenta de nuevo"<<endl;
+        cin>>a;
+    }
+    while (a>=14)
+    {
+        cout<<"El numero a calcular es demasiado grande, intenta de nuevo"<<endl;
+        cin>>a;
+    }
+    if (a>=0)
+    {
+        for (int i=1; i<=a; ++i)
+        {
+            mult=a*i;
+            fact=mult*fact;
+        }
+        cout<<fact;
+    }
+    return 0;
+}
+
+//mechipesorry
