7f530977faa4d75e9a76cc25b548e014cb241f26
Maria Belen Becerra <124795812+MaBelenBecerra@users.noreply.github.com>
2023-04-03 09:31:46 -0400

    Update ejercicio10_sol.cpp

--- practico_3/ejercicio10_sol.cpp
+++ practico_3/ejercicio10_sol.cpp
@@ -1,1 +1,24 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    //If you dance I'll dance
+    //And if you don't I'll dance anyway
+    //Give peace a chance
+    //Let the fear you have fall away
+    //I've got my eye on you
+    
+ int bill;
+ int sum = 0;
+ do {
+ cin >> bill;
+ 
+ if (bill <= 0) 
+ {
+ 
+ }else{
+ cout << "Income: " << bill << endl;
+ sum += bill;
+ }
+ } while (bill != 0);
+ cout << "Total income: " << sum << endl;
+}
