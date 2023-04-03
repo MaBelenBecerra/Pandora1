43a6a8ef22003bbb83a67588f3b88c29d89803bc
Maria Belen Becerra <124795812+MaBelenBecerra@users.noreply.github.com>
2023-04-03 10:31:16 -0400

    Create ejercicio10_sol.cpp

---
+++ practico2/ejercicio10_sol.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    //Kiss me hard before you go
+    //Summertime sadness
+    //I just wanted you to know
+    //That baby, you the best
+int num;
+    string doc;
+    cin>>num;
+    string numero=to_string(num);
+    doc=numero+" documentos encontrados :s"s;
+    cout<<doc;
+    return 0;
+}
