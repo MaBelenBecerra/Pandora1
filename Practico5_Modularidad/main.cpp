1937c3a0469821d99dfa5b7d90303ac6ae769e5e
Maria Belen Becerra <124795812+MaBelenBecerra@users.noreply.github.com>
2023-06-02 02:14:45 -0400

    Create main.cpp

---
+++ Practico5_Modularidad/main.cpp
@@ -0,0 +1,34 @@
+#include "practico.h"
+using namespace std;
+int main () {
+  cout<<"Ejercicio 1:"<<endl;
+    bot();
+  cout<<"Ejercicio 2:"<<endl;
+    change();
+   cout<<"Ejercicio 3:"<<endl;
+    middle();
+  cout<<"Ejercicio 4:"<<endl;
+    wood();
+  cout<<"Ejercicio1: "<<endl;
+  double suma = ej1();
+  cout <<"Suma: "<<suma<<endl;
+  cout<<"Ejercicio 2:"<<endl;
+  double cociente= ej2();
+	cout<<"Cociente: "<<cociente<<endl;
+  cout<<"Ejercicio 3:"<<endl;
+   double area= ej3();
+	cout<<"Rectangle area is "<<area<<endl;
+  cout<<"Ejercicio 4:"<<endl;
+    string apellido= ej4();
+    cout<<apellido<<endl;
+  cout<<"Ejercicio 5:"<<endl;
+    ej5();
+  cout<<"Ejercicio 6:"<<endl;
+    ej6();
+  cout<<"Ejercicio 7:"<<endl;
+    string docs= ej7();
+    cout<<docs<<endl;
+  cout<<"Ejercicio 8:"<<endl;
+    ej8();
+return 0;
+}
