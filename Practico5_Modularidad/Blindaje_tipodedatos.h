38cef53077c15533e6a50909c73e53f3cc89dacd
Maria Belen Becerra <124795812+MaBelenBecerra@users.noreply.github.com>
2023-06-02 01:50:06 -0400

    Create Blindaje_tipodedatos.h

---
+++ Practico5_Modularidad/Blindaje_tipodedatos.h
@@ -0,0 +1,9 @@
+#ifndef EJERCICIO1_FUNCIONES_BLINDAJEYTIPODEDATOS_H
+#define EJERCICIO1_FUNCIONES_BLINDAJEYTIPODEDATOS_H
+#include <iostream>
+using namespace std;
+void bot ();
+void change();
+void middle();
+void wood();
+#endif
